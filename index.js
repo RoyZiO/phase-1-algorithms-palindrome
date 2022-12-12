2e1d4171794178783f10b0335dbd4c77fd44d51a
RoyZiO <nziokinroy@gmail.com>
2022-12-12 03:45:43 +0300

    Completed Palindrome Lab

--- index.js
+++ index.js
@@ -1,14 +1,36 @@
 function isPalindrome(word) {
   // Write your algorithm here
+
+  const wordBackward = reversed(word);
+  if(word === wordBackward) {
+    return true;
+
+  } else {
+    return false;
+  }
 }
 
 /* 
   Add your pseudocode here
 */
-
+function reversed(word) {
+  const wordArray = word.split("")
+  const wordBackwardArray = wordArray.reverse()
+  const wordBackward= wordBackwardArray.join("");
+  return wordBackward;
+}
 /*
   Add written explanation of your solution here
-*/
+
+*/ //The first part of the solution was the pseudocode.
+// My pseudocode consisted of a function within which I declared three variables. The first variable "wordArray" was an array of the letters making up a word, arranged in a manner to correctly spell the word.
+// The second variable is an array of the reversed array of letters
+//The third variable, "wordBackward", joined the individual letters to form he reversed word, and was then returned as a string
+
+//The function isPalindrome then takes a word and compared it to its reverse, if the word is a palindrome, then it returns true, otherwise it returns false. 
+
+
+
 
 // You can run `node index.js` to view these console logs
 if (require.main === module) {
